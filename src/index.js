861d455e595078a619bb2005899653da959bd688
Flynth <florent.romdhane@gmail.com>
2024-09-07 19:46:09 +0200

    🎨 Test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const Greetings = () => {
-  return <h2>My first component</h2>;
+  return <h2>My First Component</h2>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
