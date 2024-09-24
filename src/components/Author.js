464a06e0926bf6f0055b71d20a1c0d6c2aedd7f0
Flynth <47421408+Flyntth@users.noreply.github.com>
2024-09-24 20:38:44 +0200

    💄 style: JSX CSS

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,4 +1,9 @@
 function Author() {
-  return <h4> Casey Means MD</h4>;
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+  return <h4 style={inlineHeadingStyles}> Casey Means MD</h4>;
 }
 export default Author;
