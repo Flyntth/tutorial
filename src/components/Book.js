4bfdcb423961477293b19114d6e784e1ddff7b5e
Flynth <47421408+Flyntth@users.noreply.github.com>
2024-09-26 07:00:37 +0200

    ✨ Basic props setup

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,9 @@
-function Book() {
-  const author = "Casey Means MD";
-  const title =
-    "Good Energy: The Surprising Connection Between Metabolism and Limitless Health";
+function Book(props) {
   return (
     <article className="book">
-      <img
-        src="../images/book1.jpg"
-        alt="Good Energy: The Surprising Connection Between Metabolism and Limitless Health"
-      />
-      <h2>{title}</h2>
-      <h4>{author}</h4>;
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,15 @@
 import Book from "./Book";
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
+    const author = "Casey Means MD";
+    const title =
+      "Good Energy: The Surprising Connection Between Metabolism and Limitless Health";
+    const img = "../images/book1.jpg";
+    return (
+      <section className="booklist">
+        <Book title={title} author={author} img={img} />
+        <Book title={title} author={author} img={img} />
+      </section>
+    );
 }
 export default BookList;
