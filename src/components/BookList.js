322d211b46ab1931f353e2cbbd77e6795c1c8f90
Flynth <47421408+Flyntth@users.noreply.github.com>
2024-09-26 11:48:31 +0200

    ✨ Second kinda dynamic book

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,31 @@
 import Book from "./Book";
 
 function BookList() {
-    const author = "Casey Means MD";
-    const title =
-      "Good Energy: The Surprising Connection Between Metabolism and Limitless Health";
-    const img = "../images/book1.jpg";
-    return (
-      <section className="booklist">
-        <Book title={title} author={author} img={img} />
-        <Book title={title} author={author} img={img} />
-      </section>
-    );
+  const firstBook = {
+    author: "Casey Means MD",
+    title:
+      "Good Energy: The Surprising Connection Between Metabolism and Limitless Health",
+    img: "../images/book1.jpg",
+  };
+  const secondBook = {
+    author: "Rick Riordan",
+    title:
+      "Percy Jackson and the Olympians: Wrath of the Triple Goddess (Percy Jackson & the Olympians)",
+    img: "../images/book2.jpg",
+  };
+  return (
+    <section className="booklist">
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
+    </section>
+  );
 }
 export default BookList;
