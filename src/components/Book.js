b7e0636ff16f0042ac4ece34066b2ba4b36700bb
Flynth <47421408+Flyntth@users.noreply.github.com>
2024-10-03 18:49:12 +0200

    ✨ Children prop

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,10 @@
-function Book({ author, title, img }) {
+function Book({ author, title, img, children }) {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 }
