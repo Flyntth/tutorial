ecfae84d7741e0351775a832ebeaaad7d4d6a0ac
Flynth <florent.romdhane@gmail.com>
2024-10-15 23:00:06 +0200

    ♻️ Spread operator

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,18 +8,20 @@ function BookList() {
       title:
         "Good Energy: The Surprising Connection Between Metabolism and Limitless Health",
       img: "../images/book1.jpg",
+      id: 1,
     },
     {
       author: "Rick Riordan",
       title:
         "Percy Jackson and the Olympians: Wrath of the Triple Goddess (Percy Jackson & the Olympians)",
       img: "../images/book2.jpg",
+      id: 2,
     },
   ];
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book title={book.title} author={book.author} img={book.img} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
