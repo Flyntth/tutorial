4ff1a4ada0eb8b237ad78448ec83c2bfb10f11c3
Flynth <florent.romdhane@gmail.com>
2024-10-15 21:27:09 +0200

    ♻️ Simple map

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,30 +1,26 @@
 import Book from "./Book";
 
 function BookList() {
-  const firstBook = {
-    author: "Casey Means MD",
-    title:
-      "Good Energy: The Surprising Connection Between Metabolism and Limitless Health",
-    img: "../images/book1.jpg",
-  };
-  const secondBook = {
-    author: "Rick Riordan",
-    title:
-      "Percy Jackson and the Olympians: Wrath of the Triple Goddess (Percy Jackson & the Olympians)",
-    img: "../images/book2.jpg",
-  };
+
+  const books = [
+    {
+      author: "Casey Means MD",
+      title:
+        "Good Energy: The Surprising Connection Between Metabolism and Limitless Health",
+      img: "../images/book1.jpg",
+    },
+    {
+      author: "Rick Riordan",
+      title:
+        "Percy Jackson and the Olympians: Wrath of the Triple Goddess (Percy Jackson & the Olympians)",
+      img: "../images/book2.jpg",
+    },
+  ];
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book title={book.title} author={book.author} img={book.img} />;
+      })}
     </section>
   );
 }
